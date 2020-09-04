6fa4d9f69c8fbecd04bb19a0b11b33adf4754830
Must-Student <70034845+Must-Student@users.noreply.github.com>
2020-09-04 17:23:27 +0800

    Update SendMail.py

--- SendMail.py
+++ SendMail.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# coding:utf-8
+#coding:utf-8
+
 
 import smtplib
 
@@ -8,7 +9,7 @@ from email.mime.text import MIMEText
 
 mail_user = 'roomexchange@must-student.xyz'
 
-mail_pass = 'PasswordHere'
+mail_pass = 'passwordhere'
 
 
 def send_mail(to_list, subject, content):
@@ -23,6 +24,30 @@ def send_mail(to_list, subject, content):
         s.login(mail_user, mail_pass)
         s.sendmail(me, to_list, msg.as_string())
         s.close()
+        print(to_list,subject,content)
+        return True
+    except Exception as e:
+        print (str(e))
+        with open ('error.log','a') as f:
+            f.write(str(e))
+            f.write('\n')
+
+        return False
+
+
+def send_mail_html(to_list, subject, content):
+    me = "宿舍信息共享平台" + "<" + mail_user + ">"
+    msg = MIMEText(content, 'html', 'utf-8')
+    msg['Subject'] = subject
+    msg['From'] = me
+    msg['to'] = to_list
+    try:
+        s = smtplib.SMTP_SSL("smtp.exmail.qq.com", 465)
+        #smtp地址以及端口号/SMTP address and port
+        s.login(mail_user, mail_pass)
+        s.sendmail(me, to_list, msg.as_string())
+        s.close()
+        print(to_list,subject,content)
         return True
     except Exception as e:
         print (str(e))
